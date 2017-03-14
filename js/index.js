e84007d54d4b8729dadef70215612e4c1a39f973
mvitalia <p.guglielmo@mvitalia.com>
2017-03-14 18:30:49 +0100

    s

--- js/index.js
+++ js/index.js
@@ -26,6 +26,21 @@ $(document).on("pagebeforeshow", "#login", function () {
     
 });
 
+$('#scan_read').click( function() 
+{
+    cordova.plugins.barcodeScanner.scan(
+    function (result) {
+        alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);            
+    }, 
+    function (error) {
+        alert("Scanning failed: " + error);
+    });
+    }
+);
+
 //pagina notizie
 $(document).on("pagebeforeshow", "#database", function () {
     check_login();  
