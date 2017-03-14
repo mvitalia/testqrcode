05d7ba4b953c6d3533c7639825885e85c1b63db5
mvitalia <p.guglielmo@mvitalia.com>
2017-03-14 18:25:39 +0100

    s

--- app.js
+++ app.js
@@ -111,7 +111,20 @@ var app = (function()
 		// Display refresh timer.
 		//updateTimer = setInterval(displayBeaconList, 500);
 
-		
+		$('#scan_read').click( function() 
+        {
+			cordova.plugins.barcodeScanner.scan(
+			function (result) {
+				alert("We got a barcode\n" +
+						"Result: " + result.text + "\n" +
+						"Format: " + result.format + "\n" +
+						"Cancelled: " + result.cancelled);            
+			}, 
+			function (error) {
+				alert("Scanning failed: " + error);
+			});
+			}
+		);
 	}
 
 function sincronizza_notizie(){
